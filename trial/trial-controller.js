8cc9a5c559b113852ab13f687290892586feaa94
AnastasiiaTApsy <anastasiiatierokhina@gmail.com>
2025-05-01 20:36:58 +0200

    Update trial-controller.js

--- trial/trial-controller.js
+++ trial/trial-controller.js
@@ -19,8 +19,21 @@ angular.module('pvtApp').controller('TrialCtrl', function ($scope, $state, $docu
 
 
     trialTimer.onStop.add(function (value) {
-        if (value) { $scope.data.push(value); }
-    });
+    if (value < 150) {
+        alert("Занадто швидко! Ваша реакція не зарахована.");
+        return;
+    }
+    if (value > 1000) {
+        alert("Занадто повільно! Ваша реакція не зарахована.");
+        return;
+    }
+
+    $scope.data.push(value);
+
+    if ($scope.data.length >= 20) {
+        trialTimer.disable();
+    }
+});
 
     trialTimer.onDisable.add(function () {
         var date = trialStore.save($scope.data);
